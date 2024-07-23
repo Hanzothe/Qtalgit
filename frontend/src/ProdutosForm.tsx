ba9e50ac4d7075c9cad7c042dc72fa7abfc9458e
HyggorFirmino <hyggorfirmino@gmail.com>
2024-07-23 09:23:04 -0300

    Update ProdutosForm.tsx

--- frontend/src/ProdutosForm.tsx
+++ frontend/src/ProdutosForm.tsx
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 
 export function ProdutosForm() {
   const [nome, setNome] = useState("");
   const [quantidade, setQuantidade] = useState<number>(0);
   const [message, setMessage] = useState<string>("");
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const [validated, setValidated] = useState(false)
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false){
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
     // Dados do funcionário a serem enviados
     const produtoData = {
       Nome: nome,
@@ -38,44 +44,57 @@ export function ProdutosForm() {
   };
 
   return (
-    <div className="App bg-info bg-gradient rounded-5 shadow-lg pt-4">
-      <h1 className="text-white mx-3">Adicionar Produto</h1>
-      <form className="form m-4 p-4" onSubmit={handleSubmit}>
-        <div className="row">
-          <label className="col-sm-2 col-form-label col-form-label-lg text-info-emphasis ">
-            Nome:
-          </label>
-          <div className="col-sm-10">
-            <input
-              className="form-control form-control-lg mx-n5"
-              type="text"
-              name="nome"
-              value={nome}
-              onChange={(event) => setNome(event.target.value)}
-            />
-          </div>
-        </div>
+    <>
+    <Container className="pt-4 bg-info bg-gradient rounded-5 shadow-lg">
+          <h1 className="text-white mx-3">Adicionar Produto</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <FormGroup as={Row} className="position-relative">
+              <Form.Label column="lg" sm={2} lg={2} className="mb-3 text-info-emphasis">
+                Nome:
+              </Form.Label>
+              <Col>
+              <Form.Control
+                  required
+                  type="text"
+                  name="nome"
+                  value={nome}
+                  onChange={(event) => setNome(event.target.value)}
+                  size="lg"
+                  className=""
+                />
+                <Form.Control.Feedback >
+                  Muito bom
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid" >
+                  Por favor inclua o nome do produto
+                </Form.Control.Feedback>
+              </Col>
+            </FormGroup>
+            <Row className="mt-2">
+              <Col sm={2}>
+                <Form.Label column="lg"  className="text-info-emphasis">
+                  Quantidade
+                </Form.Label>
+              </Col>
+              <Col sm={10}>
+                <Form.Control
+                  type="number"
+                  name="quantidade"
+                  value={quantidade}
+                  onChange={(event) => setQuantidade(Number(event.target.value))}
+                  size="lg"
+                />
+              </Col>
+            </Row>
 
-        <div className="row mt-2">
-          <label className="col-sm-2 col-form-label col-form-label-lg text-info-emphasis">
-            Quantidade
-          </label>
-          <div className="col-sm-10">
-            <input
-              className="form-control form-control-lg"
-              type="number"
-              name="quantidade"
-              value={quantidade}
-              onChange={(event) => setQuantidade(Number(event.target.value))}
-            />
-          </div>
-        </div>
-
-        <button type="submit" className="btn btn-lg btn-secondary">
-          Enviar
-        </button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
+            <Button type="submit" size="lg" variant="secondary">
+              Enviar
+            </Button>
+            
+          </Form>
+          {message && <p>{message}</p>}
+        </Container>
+    </>
+    
   );
 }
