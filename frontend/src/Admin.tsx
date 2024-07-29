e265f70dd88730db8879a2fc75ade5ac8c6ef851
HyggorFirmino <hyggorfirmino@gmail.com>
2024-07-29 10:15:16 -0300

    Criação do admin

---
+++ frontend/src/Admin.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { AdminUI, Resource } from "react-admin"
+
+// const dataProvider = json
+
+export function Admin() {
+    return(
+        <Admin>
+
+        </Admin>
+    );
+}
