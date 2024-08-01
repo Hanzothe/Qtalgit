eefb900baec79a47ec5f83b600ac95a1705c57f3
HyggorFirmino <hyggorfirmino@gmail.com>
2024-08-01 15:49:56 -0300

    Utilização do LoginPopUp

--- frontend/src/LoginPopUp.tsx
+++ frontend/src/LoginPopUp.tsx
@@ -1,124 +1,29 @@
-import axios from 'axios';
 import React, { useState } from 'react';
-import { Modal, Button, Form, Col, InputGroup, Row } from 'react-bootstrap';
-import "./login.css"
+import { Modal, Button, Form, InputGroup, Row } from 'react-bootstrap';
+import "./login.css";
 
-function LoginPopUp() {
-  
-
-  const [selectedFuncionario, setSelectedFuncionario] = useState("");
-  const [arquivo, setArquivo] = useState<File | null>(null);
-  const [fileUrl, setFileUrl] = useState("");
-  const [message, setMessage] = useState<string>("");
-
-  const [show, setShow] = useState(false);
+function LoginPopUp({ show, onHide }) {
   const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState('');
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   const handleTogglePassword = () => setShowPassword(!showPassword);
-
-  
-
-
-  // useEffect(() => {
-  //   // Buscar os funcionários do banco de dados
-  //   const fetchFuncionarios = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         "http://localhost:3000/get-funcionarios"
-  //       );
-  //       setFuncionarios(response.data);
-  //     } catch (error) {
-  //       console.error("Erro ao buscar funcionários:", error);
-  //     }
-  //   };
-
-  //   fetchFuncionarios();
-  // }, []);
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setArquivo(event.target.files[0]);
-    }
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (!arquivo) {
-      setMessage("Nenhum arquivo selecionado");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", arquivo);
-
-    try {
-      // Primeiro envia o arquivo para a rota /upload
-      const fileResponse = await axios.post(
-        "http://localhost:3000/upload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      console.log("Arquivo enviado:", fileResponse.data);
-      setFileUrl(`http://localhost:3000/files/${fileResponse.data}`);
-
-      // Envia os dados da nota para a rota /add-nota
-      const notaData = {
-        funcionario: selectedFuncionario,
-        fileUrl: `http://localhost:3000/files/${fileResponse.data}`, // URL do arquivo
-      };
-
-      const notaResponse = await axios.post(
-        "http://localhost:3000/add-nota",
-        notaData,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      console.log("Nota adicionada:", notaResponse.data);
-      setMessage("Nota adicionada com sucesso");
-    } catch (error) {
-      console.error("Erro:", error);
-      setMessage("Erro ao enviar o arquivo ou adicionar a nota");
-    }
-  };
 
   return (
     <>
-      <div className={show ? 'blur-background' : ''}>
-      <Button variant="primary" onClick={handleShow}>
-        Open Login Modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose} centered>
+      <Modal show={show} onHide={onHide} centered>
         <Modal.Body className="login-modal-body">
           <h2 className="text-center mb-4">Login</h2>
-          <Form noValidate onSubmit={handleSubmit}>
+          <Form noValidate>
             <Form.Group className="mb-3" controlId="formUsername">
               <InputGroup>
-                <InputGroup.Text>
-                 @
-                </InputGroup.Text>
+                <InputGroup.Text>@</InputGroup.Text>
                 <Form.Control type="text" placeholder="Username" required />
               </InputGroup>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formPassword">
               <InputGroup>
-                <InputGroup.Text>
-                 @
-                </InputGroup.Text>
+                <InputGroup.Text>@</InputGroup.Text>
                 <Form.Control
                   type={showPassword ? 'text' : 'password'}
                   placeholder="Password"
@@ -147,12 +52,11 @@ function LoginPopUp() {
             </Row>
 
             <Row className="mt-4 text-center">
-              <span>Não possue conta? <a href="#" className="text-info">Registrar</a></span>
+              <span>Não possui conta? <a href="#" className="text-info">Registrar</a></span>
             </Row>
           </Form>
         </Modal.Body>
       </Modal>
-    </div>
     </>
   );
 }
--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -6,10 +6,12 @@ import logo from "./images/images (1).png";
 import { Card } from "react-bootstrap";
 import card1image from './images/office.jpg'
 import card2image from './images/prestador.jpg'
+import LoginPopUp from './LoginPopUp'; // Importe o componente LoginPopUp
 
 const Home = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [showAdditionalContent, setShowAdditionalContent] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false); // Estado para o modal de login
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -21,56 +23,45 @@ const Home = () => {
     // Remover esta linha para que o conteúdo adicional não desapareça ao sair do logo
   };
 
+  const openLoginModal = () => setShowLoginModal(true); // Função para abrir o modal
+
   return (
     <div className="background-image">
       <div className="overlay"></div>
       <div className="content">
         <div className={`additional-content ${showAdditionalContent ? 'show' : ''}`}>
-        <Card className="box box1">
-        <Card.Img variant="top" src={card1image}  style={{ boxShadow: 'rgba(0, 0, 0, 0.75) 0px 3px 12px 2px'}}
-/>
-        <Card.Body>
-          <Card.Title>Administração</Card.Title>
-          <Card.Text>
-            Clique aqui para ter acesso a administração de notas fiscais e funcionários.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">É necessário fazer login</small>
-        </Card.Footer>
-      </Card>
-          {/* <Card className="box box1">
-          <Card.Img src="./images/office2.jpeg" alt="Card image" />
+          <Card className="box box1" onClick={openLoginModal}> {/* Adicione onClick */}
+            <Card.Img variant="top" src={card1image}  style={{ boxShadow: 'rgba(0, 0, 0, 0.75) 0px 3px 12px 2px'}} />
             <Card.Body>
-              <Card.Text className="fs-1 fw-bold">
-                Sou administrador
+              <Card.Title>Administração</Card.Title>
+              <Card.Text>
+                Clique aqui para ter acesso a administração de notas fiscais e funcionários.
               </Card.Text>
             </Card.Body>
-          </Card> */}
+            <Card.Footer>
+              <small className="text-muted">É necessário fazer login</small>
+            </Card.Footer>
+          </Card>
         </div>
-        <div
-          className={`animate-div ${isHovered ? 'hovered' : ''}`}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
+        <div className={`animate-div ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           <img src={logo} alt="Logo" className="logo-image" />
         </div>
         <div className={`additional-content ${showAdditionalContent ? 'show' : ''}`}>
-        <Card className="box box2">
-        <Card.Img variant="top" src={card2image} style={{   boxShadow: 'rgba(0, 0, 0, 0.75) 0px 3px 12px 2px'}}
-/>
-        <Card.Body>
-          <Card.Title>Estoque</Card.Title>
-          <Card.Text>
-            Aqui você pode fazer gestão e consultas do estoque. 
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">É necessário fazer login</small>
-        </Card.Footer>
-      </Card>
+          <Card className="box box2" onClick={openLoginModal}> {/* Adicione onClick */}
+            <Card.Img variant="top" src={card2image} style={{   boxShadow: 'rgba(0, 0, 0, 0.75) 0px 3px 12px 2px'}}/>
+            <Card.Body>
+              <Card.Title>Estoque</Card.Title>
+              <Card.Text>
+                Aqui você pode fazer gestão e consultas do estoque. 
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">É necessário fazer login</small>
+            </Card.Footer>
+          </Card>
         </div>
       </div>
+      <LoginPopUp show={showLoginModal} onHide={() => setShowLoginModal(false)} /> {/* Renderize o modal */}
     </div>
   );
 };
